8a4fdfd2184207b7879785f76ecd5eba65343905
Tuan Bui <tuanba1@gmail.com>
2024-11-11 12:41:20 +0700

    add support for dotfiles folder (.config and home)

--- dotfiles.nix
+++ dotfiles.nix
@@ -1,10 +1,43 @@
 { config, pkgs, ... }:
-{
-  # import `dotfiles` dir into ~/.config
+ {
+  # import `dotfiles config` dir into ~/.config
   home.file."${config.xdg.configHome}" = {
-    source = ./dotfiles;
+    source = ./dotfiles/.config;
     recursive = true;
   };
 
+ 
+# some file to home folder 
+# TODO: make it autoscan 
+# home.file = builtins.attrsets.mapAttrs (n: v: {
+#   source = ./dotfiles/"${n}";
+#   recursive = true;
+# }) (builtins.readDir ./dotfiles/);
+
+  # vrapper config for vim keybindings in eclipse base editors
+  # like DBEaver
+  home.file.".vrapperrc" = {
+    source = ./dotfiles/home/.vrapperrc;
+    recursive = true;
+  };
+
+  home.file.".xkb" = {
+    source = ./dotfiles/home/.xkb;
+    recursive = true;
+  };
 
 }
+
+
+
+# let
+#   dotfiles = pkgs.lib.attrsets.mapAttrsToList (n: v: {
+#     target = ".${n}";
+#     source = "${./dotfiles}/${n}";
+#     # recursive = true;
+#   }) (builtins.readDir ./dotfiles);
+# in
+# {
+#   builtins.trace "dotfiles:" "dot" ;
+#   home.file = builtins.listToAttrs dotfiles;
+# }
